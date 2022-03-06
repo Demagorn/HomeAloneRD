f92d061a23dfdbd32147dd499282e8bdd87911c8
Serg Gor <pyprosg@gmail.com>
2022-03-06 12:25:06 +0200

    Assignment 2 done. Read me updates

--- Assigmnet2.py
+++ Assigmnet2.py
@@ -6,11 +6,26 @@ This function should random n times values for A, B and C and evaluate the formu
  For each evaluation print the formula and the result
  Example:
 """
+import random as r
 
-@wraps
-func.__name__ = wrapper.__name__
 
-MyFunc(formula="2*A+B",n=2)
+def test_value():
+    yield r.randint(-100,100)
+
+
+def formula_validation(formula, n=100):
+    variables = ["A","B","C"]
+    for _ in range(n):
+        for v in variables:
+            print(formula)
+            formula = formula.replace(v, str(next(test_value())))
+        print(formula)
+        result = eval(formula)
+
+        print(formula + "=" + str(result))
+
+
+formula_validation(formula="2*A+B",n=2)
 # Output (possible):
 # 2*3+5 = 11
 # 2*12+49 = 73
--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
 1.
 Since there is no information regarding the source of the input there must be a validation for the input
 data. It can be ether wrong type data or non-possible manipulation that can cause a crash of the function.
+
+2. Initial assumed that i've been told on this assignment that the input function is a valid one will not crush me.
+There is no limitation on using additional functions so its @wraps and not "func.__name__ = wrapper.__name__"
+
